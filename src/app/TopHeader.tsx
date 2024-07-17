143d33176cdd9b9ff5c3610a77688147eda88e09
hengin-eer <hirokix0608@gmail.com>
2024-07-17 14:25:23 +0900

    ✨ feat: トップページのヘッダーを作成 画像と見出しテキスト

---
+++ src/app/TopHeader.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import { FC } from "react";
+
+export const TopHeader: FC = () => {
+    return (
+        <header className="pt-10 flex flex-col items-center">
+            <Image
+                className="lg:w-[400px] sm:w-[300px] drop-shadow-lg"
+                width={1000}
+                height={750}
+                src="/logo.png"
+                alt="Tako Talk Hero Image"
+            />
+            <h1 className="pt-8 text-4xl font-bold">Tako Talk | タコさんとフリー雑談</h1>
+        </header>
+    )
+}
