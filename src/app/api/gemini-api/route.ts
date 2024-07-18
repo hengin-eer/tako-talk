7488361904b896bcf47f96b9adb33f713e543dec
hengin-eer <hirokix0608@gmail.com>
2024-07-18 16:24:50 +0900

    ✨ feat: エラーハンドリングの追加、リクエストの値の取り方を変更

--- src/app/api/gemini-api/route.ts
+++ src/app/api/gemini-api/route.ts
@@ -10,32 +10,43 @@ const genAI = new GoogleGenerativeAI(apiKey as string);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
-  systemInstruction: "あなたはディズニーのタートルトークです。語尾を「タコォ～」", // プロンプトでっす
+  systemInstruction: "あなたは哲学者ソクラテスです。ディズニーのタートルトークで働いています。カメのクラッシみたいなマイペースで馴れ馴れしい口調で面白い回答をして", // プロンプトでっす
 });
 
 const generationConfig = {
   temperature: 0.5,
   topP: 0.95,
   topK: 64,
-  maxOutputTokens: 8192,
+  maxOutputTokens: 300,
   responseMimeType: "text/plain",
 };
 
-type Req = {
-	message: string;
-}
+export async function POST(req: Request) {
+  const { message } = await req.json();
+  console.log("Received message:", message);
 
-async function POST(req: { body: Req }, res: Response) {
-	const chatMessage: string = req.body.message;
   const chatSession = model.startChat({
+    history: [],
     generationConfig,
   });
 
-  const result = await chatSession.sendMessage(chatMessage); // チャットの送信メッセージ
-  const response = result.response;
-	console.log(response.text());
-	
-	return NextResponse.json({
-		message: response.text()
-	});
+  try {
+    const result = await chatSession.sendMessage(message); // チャットの送信メッセージ
+    console.log("SendMessage result:", result);
+
+    const response = result.response;
+    console.log("Response:", response);
+
+    return NextResponse.json({
+      message: response.text(),
+    });
+  } catch (error) {
+    console.error("Error sending message:", error);
+    return NextResponse.json(
+      {
+        error: "Failed to send message",
+      },
+      { status: 500 }
+    );
+  }
 }
