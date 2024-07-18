e7db89a4bca457681836f500ec2c8ad4c013677d
hengin-eer <hirokix0608@gmail.com>
2024-07-18 10:06:49 +0900

    ✨ feat: 取り敢えずモデルのコンポーネントを作成 まだモデルは無く、ボックスを表示しているだけ

---
+++ src/app/chat/OctopusModel.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react"
+
+export const OctopusModel: FC = () => {
+    return (
+        <div className="flex items-center justify-center w-[400px] h-[400px] rounded-md bg-white">takotako 3D model</div>
+    )
+}
