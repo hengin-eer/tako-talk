5a9e2834f001268758a5c49173dc5273344d5964
hengin-eer <hirokix0608@gmail.com>
2024-07-18 16:30:10 +0900

    ✨ feat: タコの絵文字を追加しただけ🐙

--- src/app/chat/OctopusModel.tsx
+++ src/app/chat/OctopusModel.tsx
@@ -2,6 +2,6 @@ import { FC } from "react"
 
 export const OctopusModel: FC = () => {
     return (
-        <div className="flex items-center justify-center w-[400px] h-[400px] rounded-md bg-white">takotako 3D model</div>
+        <div className="flex items-center justify-center w-[400px] h-[400px] rounded-md bg-white text-[240px]">🐙</div>
     )
 }
