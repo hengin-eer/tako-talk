a36c9351fe031c1ec42b980ebadc935e768ccc73
hengin-eer <hirokix0608@gmail.com>
2024-11-01 14:54:52 +0900

    ✨ #5 旧モデルだけど位置調整、光源、操作等々

--- src/app/chat/ModelArea.tsx
+++ src/app/chat/ModelArea.tsx
@@ -3,16 +3,17 @@
 import { Canvas } from "@react-three/fiber";
 import { FC } from "react";
 import { OctopusModel } from "./OctopusModel";
+import { OrbitControls } from "@react-three/drei";
 
 export const ModelArea: FC = () => {
 	return (
 		<div className="w-full sm:w-[400px] aspect-square mx-auto">
-			<Canvas>
-				<ambientLight />
+			<Canvas camera={{ position: [0, 0, 10], fov: 30 }}>
+				<OrbitControls enableRotate={true} enableZoom={true} />
+				<ambientLight intensity={2} />
 				{/* Rig? */}
 				<fog attach="fog" color={"#fff"} near={1} far={20} />
-				<pointLight position={[10, 10, 10]} />
-				<OctopusModel position={[0, 0, 2]} />
+				<OctopusModel position={[0, -1.5, 2]} />
 			</Canvas>
 		</div>
 	)
