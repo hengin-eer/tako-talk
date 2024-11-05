c920cc76d68bc01288b373dabbf82ea87416daa1
hengin-eer <hirokix0608@gmail.com>
2024-11-06 01:16:53 +0900

    ✨ #2 `ChatArea` → `ChatForm`に変更

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,6 +1,7 @@
 import { ChatArea } from "./ChatArea";
 import { Metadata } from "next";
 import { ModelArea } from "./ModelArea";
+import ChatForm from "./ChatForm";
 
 export const metadata: Metadata = {
 	title: "チャットを始める | Tako Talk",
@@ -11,7 +12,8 @@ export default function ChatPage() {
 	return (
 		<main className="min-h-[100svh] h-full pt-5 pb-8 flex flex-col items-center justify-between">
 			<ModelArea />
-			<ChatArea />
+			{/* <ChatArea /> */}
+			<ChatForm />
 		</main>
 	)
 }
