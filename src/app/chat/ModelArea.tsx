070757f3128721a2f46bbce8aa2dee859e733199
hengin-eer <hirokix0608@gmail.com>
2024-11-06 15:42:33 +0900

    ✨ #4 モデルを表示、微調整するためのコンポ作成

--- src/app/chat/ModelArea.tsx
+++ src/app/chat/ModelArea.tsx
@@ -2,19 +2,23 @@
 
 import { Canvas } from "@react-three/fiber";
 import { FC } from "react";
-import { OctopusModel } from "./OctopusModel";
 import { OrbitControls } from "@react-three/drei";
+import Model from "./NewTako";
+import { ActionName } from "@/types/Model";
 
-export const ModelArea: FC = () => {
+type Props = {
+	actionName: ActionName;
+};
+
+export const ModelArea: FC<Props> = ({ actionName }) => {
 	return (
-		<div className="w-full sm:w-[400px] aspect-square mx-auto">
+		<div className="mt-[160px] mx-auto h-[60vh]">
 			<Canvas camera={{ position: [0, 0, 10], fov: 30 }}>
 				<OrbitControls enableRotate={true} enableZoom={true} />
-				<ambientLight intensity={2} />
-				{/* Rig? */}
-				<fog attach="fog" color={"#fff"} near={1} far={20} />
-				<OctopusModel position={[0, -1.5, 2]} />
+				<ambientLight intensity={1} color={"#a0d8ef"} />
+				<fog attach="fog" color={"#2288aa"} near={3} far={20} />
+				<Model actionName={actionName} />
 			</Canvas>
 		</div>
-	)
-}
+	);
+};
